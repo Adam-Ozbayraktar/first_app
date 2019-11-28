9b3d127863704aedcd686b4dc955fd22e3695f22
AdamOz <adamozbayraktar@live.com>
2019-11-28 13:23:42 +0200

    Split widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart'; 
+
 // void main() {
 //   runApp(MyApp());
 // }
@@ -11,19 +13,18 @@ class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> {
 
-  var questionIndex = 0;
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState((){
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
-    
   }
 
   @override
@@ -40,12 +41,12 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(
-              questions[questionIndex],
+            Question(
+              questions[_questionIndex],
             ),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
@@ -53,7 +54,7 @@ class MyAppState extends State<MyApp> {
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
           ],
         ),
